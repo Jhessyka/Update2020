1131bb8c678e7e1dba1e579dfe19c9129a62e8ad
Jessica de Souza <jhessicadesouza12@hotmail.com>
2021-03-24 16:30:03 -0300

    Projeto criado

---
+++ SaveWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SaveWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
